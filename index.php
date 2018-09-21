49cf1a4738c3f4a07e23d6dfd59b4e0cf1ef223e
Gabriel Sanchez <gsanchez@csumb.edu>
2018-09-21 20:31:31 +0000

    initial changes

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE> 
+<!DOCTYPE html>
+<html>
+    <head>
+        <h1 id= "title">
+            SilverJack Card Game
+        </h1>
+        <br />
+    </head>
+    
+    <body>
+     <form>
+          <input type="submit" value=" Play Again! " /> 
+          
+    </form>
